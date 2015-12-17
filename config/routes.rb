846e381a642e85823b5067bf8140eb25a3c4f7df
Jessie Lin <jessie.lin@procore.com>
2015-12-17 15:47:21 -0800

    Finish log in/log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  root  'static_pages#home'
 
-  get 'users/new'
+  get   'help'    =>  'static_pages#help'
 
-  root 'static_pages#home'
+  get   'about'   =>  'static_pages#about'
 
-  get 'help'    =>  'static_pages#help'
+  get   'contact' =>  'static_pages#contact'
 
-  get 'about'   =>  'static_pages#about'
+  get   'signup'  =>  'users#new'
 
-  get 'contact' =>  'static_pages#contact'
-
-  get 'signup'  =>  'users#new'
+  get   'login'   =>  'sessions#new'
+  post  'login'   =>  'sessions#create'
+  delete 'logout' =>  'sessions#destroy'
 
   resources :users
 
